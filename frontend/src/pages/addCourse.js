131953d22ed9bcdcfe703011076ac4bfcdb8b9b7
harshulgupta05 <65627692+harshulgupta05@users.noreply.github.com>
2022-04-30 19:20:32 -0400

    updated addCourse

--- frontend/src/pages/addCourse.js
+++ frontend/src/pages/addCourse.js
@@ -1,7 +1,7 @@
 import { React, Component, useState, setState } from "react";
 import { Router, Link, useLocation } from "wouter";
 
-function addCourse() {
+function AddCourse() {
     const [location, setLocation] = useLocation();
 
     const addCourse = () => {
@@ -24,4 +24,25 @@ function addCourse() {
             }
         });
     }
+
+    return (
+        <div>
+            <div className="w-100 text-center ml-auto mr-auto d-flex justify-content-center pt-5 align-items-center">
+                <div className="register">
+                    <h2 className="display-2 text-center">
+                        Add a Course
+                    </h2>
+                    <div className="form-group text-center">
+                        <label for="User ID" className="mx-2 form-label">Course Code (e.g. SCH4U)</label>
+                        <input type="text" id="userid" placeholder="######" className="m-auto pl-3 form-control w-75" />
+                    </div>
+                    <div className="text-center">
+                        <button onClick={addCourse} type="submit" className="btn btn=primary text-center m-2">Add Course</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
+
+export default AddCourse;
