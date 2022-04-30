bad11a0cfab532d8b76271c9fe475e482e8a8d32
harshulgupta05 <65627692+harshulgupta05@users.noreply.github.com>
2022-04-30 18:19:57 -0400

    updated routing

--- frontend/src/components/router.jsx
+++ frontend/src/components/router.jsx
@@ -1,9 +1,13 @@
 import * as React from "react";
 import { Switch, Route, Router } from "wouter";
 import SignUp from "../pages/signup";
+import Home from "../pages/home";
+import Login from "../pages/login";
 
 export default () => {
     <Switch>
+        <Route path="/" component={Home} />
         <Route path="/signup" component={SignUp} />
+        <Route path="login" component={Login} />
     </Switch>
 }
