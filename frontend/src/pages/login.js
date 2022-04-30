d671f571332f6bce2162d2fb054d122738f17881
harshulgupta05 <65627692+harshulgupta05@users.noreply.github.com>
2022-04-30 17:57:52 -0400

    updated login page

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,36 +1,56 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import LoginForm from './components/LoginForm';
+import { React, Component, useState, setState } from "react";
+import { Router, Link, useLocation } from "wouter";
 
-function App() {
-    const adminUser = {
-        email: "peter.lee1386@gmail.com",
-        password: "admin123"
-    }
+function Login() {
+    const [location, setLocation] = useLocation();
 
-    const [user, setUser] = useState({ name: "", email: "" });
-    const [error, setError] = useState("");
+    const login = () => {
+        const userid = document.getElementById("userid").value;
+        const password = document.getElementById("pw").value;
 
-    const Login = details => {
-        console.log(details);
-    }
-    const Logout = () => {
-        console.log("Logout");
+        fetch("http://localhost:5000login", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: {
+                "number": userid,
+                "password": password
+            }
+        }).then(response => response.json()).then((data) => {
+            if (data.success === true) {
+                sessionStorage.setItem("user", userid);
+                sessionStorage.setItem("school", data.school);
+            }
+            else {
+                alert("Login failed. Please try again.");
+            }
+        });
     }
 
     return (
-        <div className="login">
-            {(user.email != "") ? (
-                <div className="welcome">
-                    <h2>Welcome, <span>{user.name}</span></h2>
-                    <button>Logout</button>
+        <div>
+            <div className="w-100 text-center ml-auto mr-auto d-flex justify-content-center pt-5 align-items-center">
+                <div className="register">
+                    <h2 className="display-2 text-center">
+                        Login to ClassMate!
+                    </h2>
+                    <div className="form-group text-center">
+                        <label for="User ID" className="mx-2 form-label">User ID (please use your Student #)</label>
+                        <input type="text" id="userid" placeholder="######" className="m-auto pl-3 form-control w-75" />
+                    </div>
+                    <div className="form-group text-center">
+                        <label for="Password" className="mx-2 form-label">Password</label>
+                        <input type="password" id="pw" placeholder="Password" className="m-auto pl-3 form-control w-75" />
+                    </div>                    
+                    <div className="text-center">
+                        <button onClick={login} type="submit" className="btn btn=primary text-center m-2">Sign Up</button>
+                    </div>
                 </div>
-            ) : (
-                <LoginForm />
-            )}
+            </div>
         </div>
     );
 }
 
-export default LoginForm;
+export default Login;
