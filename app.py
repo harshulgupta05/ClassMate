9af287714afc5b7860975bfa4072cc7592ccd1d7
harshulgupta05 <65627692+harshulgupta05@users.noreply.github.com>
2022-04-29 22:00:05 -0400

    added login and signup to API

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, request, redirect, url_for, current_app
 from flask_mongoengine import MongoEngine
 from dotenv import load_dotenv
 from pymongo import MongoClient
@@ -8,13 +8,43 @@ import os
 
 app = Flask(__name__)
 
+# load URI from dotenv
 load_dotenv()
-
 MONGODB_URI = os.environ['MONGODB_URI']
 
+# connect to database
 try:
     client = MongoClient(MONGODB_URI)
     print("Database connected.")
 except Exception as e:
     print("Database not connected.")
 
+# navigate to the ClassMate database
+db = client['ClassMate']
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    content = request.get_json(force=True)
+    col = db['users']
+    dict = { "studentnumber": content["number"], "password": content["password"], "school": content["school"] }
+
+    insert = col.insert_one(dict)
+
+    # return redirect(url_for(app.courses))
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    content = request.get_json(force=True)
+
+    col = db['users']
+    pw = col.find_one({"studentnumber": content.number }, {"password": 1})
+    if (content.password == pw["password"]):
+        return jsonify({ "studentnumber": content.number, "success" : True })
+
+@app.route("/<schoolid>/courses")
+def classes(schoolid):
+    col = db[schoolid]
+
+if __name__ == "__main__":
+    app.run(debug=True)
